644695c2c30a7cb3dac7c3582562b57d86e8bc52
allenwhitedev <allenwhitedev@gmail.com>
2016-11-29 22:20:43 -0500

    Add 'study group finder' to home page

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Study Group Finder React Home</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is the home page for the StudyGroupFinder React App. To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
