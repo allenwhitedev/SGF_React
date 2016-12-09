b7680be708a649842be39fa2b75867d17fec753d
allenwhitedev <allenwhitedev@gmail.com>
2016-12-09 06:09:39 -0500

    Style groups page

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
 
+import gravatar1 from '../img/gravatar1.png'
+import gravatar2 from '../img/gravatar2.png'
+import gravatar3 from '../img/gravatar3.png'
+import gravatar4 from '../img/gravatar4.png'
+import gravatar5 from '../img/gravatar5.png'
+
 class Groups extends Component
 {
+
+  constructor(props)
+  {
+    super(props)
+  }
+
+  getGravatar(gravatar)
+  {
+    if (gravatar === "gravatar1")
+      return gravatar1
+    else if (gravatar === "gravatar2")
+      return gravatar2
+    else if (gravatar === "gravatar3")
+      return gravatar3
+    else if (gravatar === "gravatar4")
+      return gravatar4
+    else if (gravatar === "gravatar5")
+      return gravatar5                      
+  }
+
+  formatDate(date) 
+  {
+    let formattedDate = ""
+    formattedDate
+  }
+
 
   render()
   {
@@ -10,42 +42,91 @@ class Groups extends Component
       {
         _id: "a83jgdfsdf",
         "name": "Super Studiers",
+        "class": "CEN4010",
+        "description": "We're the best at studying, trust us",
         members: ["a83jsj9lsdfj", "b38k2jf0jjsdf"],
-        meetingTimes: ["ISODate(2016-12-04T03:03:05.908Z)"],
+        meetingTimes: ["Mon 11:30am, Wed 12:30pm"],
         locationName: "EE 106",
         location: ["lat": 33.556828283, "lng": -77.596928386],
-        gravatar: "gravatar3",
+        gravatar: "gravatar1",
         lastActivityAt: "ISODate(2016-11-26T00:32:48.745Z)",
         createdAt: "ISODate(2016-11-26T00:32:48.745Z)"
       },
       {
       _id: "b2j5k3k4j3",
-      "name": "Most Valuble Studiers",
+      "name": "Most Valuable Studiers",
+      "class": "CEN4010",
+      "description": "We are all MVPs of getting good grades",
       members: ["a83jsj9lsdfj", "c4j2jckdjdj2"],
-      meetingTimes: ["ISODate(2016-14-04T03:03:05.908Z)"],
+      meetingTimes: ["Tues 9:15am, Thurs 1:00pm"],
+      locationName: "EE 106",
+      location: ["lat": 33.556828283, "lng": -77.596928386],
+      gravatar: "gravatar2",
+      lastActivityAt: "ISODate(2016-15-26T00:32:48.745Z)",
+      createdAt: "ISODate(2016-10-22T00:32:48.745Z)"
+      },
+      {
+      _id: "b2j5k3k4j3",
+      "name": "Sleepy Students And Staff",
+      "class": "COP4020",
+      members: ["a83jsj9lsdfj", "c4j2jckdjdj2"],
+      meetingTimes: ["Tues 11:15am, Thurs 3:00pm"],
       locationName: "EE 106",
       location: ["lat": 33.556828283, "lng": -77.596928386],
       gravatar: "gravatar3",
       lastActivityAt: "ISODate(2016-15-26T00:32:48.745Z)",
       createdAt: "ISODate(2016-10-22T00:32:48.745Z)"
-      }
+      },
+      {
+      _id: "b2j5k3k4j3",
+      "name": "Futbol Fans",
+      "class": "COP4020",
+      members: ["Tues 3:30pm, Fri 8:30am"],
+      meetingTimes: ["ISODate(2016-14-04T03:03:05.908Z)"],
+      locationName: "EE 106",
+      location: ["lat": 33.556828283, "lng": -77.596928386],
+      gravatar: "gravatar4",
+      lastActivityAt: "ISODate(2016-15-26T00:32:48.745Z)",
+      createdAt: "ISODate(2016-10-22T00:32:48.745Z)"
+      },
+      {
+      _id: "b2j5k3k4j3",
+      "name": "Crazy Kids",
+      "class": "COP4020",
+      members: ["a83jsj9lsdfj", "c4j2jckdjdj2"],
+      meetingTimes: ["Sat 12:00am, Sun 1:00pm"],
+      locationName: "EE 106",
+      location: ["lat": 33.556828283, "lng": -77.596928386],
+      gravatar: "gravatar5",
+      lastActivityAt: "ISODate(2016-15-26T00:32:48.745Z)",
+      createdAt: "ISODate(2016-10-22T00:32:48.745Z)"
+      },
     ]
 
+
     let groupsDisplay = dummyGroups.map((dummyGroups,index) =>
-      <div key={index} className="group-searched">
-        {dummyGroups.name}<br/>
-        <section>
-          <img src='http://image.flaticon.com/icons/png/512/27/27825.png' alt="" height="15" width="15"/> {dummyGroups.members.length} members<br/>
-          {dummyGroups.meetingTimes}<br/>
-          {dummyGroups.locationName}<br/>
+      <li key={index} className="group boxShadow1">
+        <section className="groupTextInfo"> 
+          <h2 className="groupName verticalAlignFlex"> <img className="gravatarSmall marginRight10px" src={this.getGravatar(dummyGroups.gravatar)} alt=""/> <span> {dummyGroups.name} - {dummyGroups.class} </span> </h2>
+          <h3>{dummyGroups.description}</h3>
         </section>
-      </div>
+
+        <ul className="groupInfo tabPlusOnly fullWidth flexHorizontalLayout">
+          <li className="groupMembers verticalAlignFlex"> <i className="material-icons">group</i> &nbsp; {dummyGroups.members.length} members </li>
+          <li className="meetingTimes verticalAlignFlex"> <i className="material-icons">access_time</i> &nbsp; {dummyGroups.meetingTimes} </li>
+          <li className="locationName verticalAlignFlex"> 
+            <i className="material-icons">place</i> &nbsp;
+            <span> {dummyGroups.locationName} </span> 
+          </li>
+        </ul>
+      </li>
     )
 
     return(
-      <div>
-        {groupsDisplay}
-      </div>
+      <ul className="groups">
+         <h1 className="textCenter">Groups </h1>
+          {groupsDisplay}
+      </ul>
       // Groups Display
     )
   }
