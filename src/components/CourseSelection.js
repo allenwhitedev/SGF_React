32bac1385ad76976ed5809537554ef97064110da
allenwhitedev <allenwhitedev@gmail.com>
2016-12-08 21:58:38 -0500

    Fix remove course functionality from course selection page and map groups from json for testing purposes on selection page

--- src/components/CourseSelection.js
+++ src/components/CourseSelection.js
@@ -10,7 +10,34 @@ class CourseSelection extends Component
     this.handleOnSubmit = this.handleOnSubmit.bind(this)
     this.handleOnRemove = this.handleOnRemove.bind(this)
     this.handleSubmitCourses = this.handleSubmitCourses.bind(this)
-    this.state = {courses: [], newCourse: ""}
+    
+    let myGroups =
+    [
+      {
+        _id: "a83jgdfsdf",
+        "name": "Super Studiers",
+        members: ["a83jsj9lsdfj", "b38k2jf0jjsdf"],
+        meetingTimes: ["ISODate(2016-12-04T03:03:05.908Z)"],
+        locationName: "EE 106",
+        location: ["lat": 33.556828283, "lng": -77.596928386],
+        gravatar: "gravatar3",
+        lastActivityAt: "ISODate(2016-11-26T00:32:48.745Z)",
+        createdAt: "ISODate(2016-11-26T00:32:48.745Z)" 
+      },
+      {
+      _id: "b2j5k3k4j3",
+      "name": "Most Valuble Studiers",
+      members: ["a83jsj9lsdfj", "c4j2jckdjdj2"],
+      meetingTimes: ["ISODate(2016-14-04T03:03:05.908Z)"],
+      locationName: "EE 106",
+      location: ["lat": 33.556828283, "lng": -77.596928386],
+      gravatar: "gravatar3",
+      lastActivityAt: "ISODate(2016-15-26T00:32:48.745Z)",
+      createdAt: "ISODate(2016-10-22T00:32:48.745Z)" 
+      }
+    ]
+
+    this.state = {courses: [], newCourse: "", groups: myGroups}
 	}
 
   handleSubmitCourses(event)
@@ -18,10 +45,11 @@ class CourseSelection extends Component
     alert("Submit Courses, assuming for now submit was successful")
   }
 
-  handleOnRemove(index)
+  handleOnRemove(event)
   {
+    let index = event.target.id
     this.state.courses.splice(index, 1);
-    this.setState(this.state);
+    this.setState(this.state); 
   }
 
   handleOnChange(event)
@@ -42,11 +70,11 @@ class CourseSelection extends Component
     let submitButton = null;
 
     (this.state.courses.length > 0)
-    ?
+    ? // true
       submitButton = <button className="submitButton textCenter">
         Take Me To Groups
       </button>
-    :
+    : // false
       submitButton = <button className="submitButton textCenter" disabled="true" onSubmit={this.handleSubmitCourses}>
         Take Me To Groups
       </button>
@@ -57,14 +85,26 @@ class CourseSelection extends Component
     let coursesDisplay = courses.map( (course, index) =>
       <li key={index} className="course">
         {course}
-        <a className="remove-course" onClick={this.handleOnRemove}>✖</a>
+        <a className="remove-course" id={index} onClick={this.handleOnRemove}>✖</a>
       </li>
+    )
+
+    let groups = this.state.groups
+    
+    let groupsDisplay = groups.map( (group, index) =>
+      <li key={index} className="group">
+        {group.name}
+      </li>
+
     )
 
     return(
       <section className="courseSelection">
 
-        {/* Course Display */}
+        {/* Groups Display */}
+        <ul className="groups textCenter"> {groupsDisplay} </ul>
+
+        {/* Courses Display */}
         <ul className="courses textCenter"> {coursesDisplay} </ul>
 
         {/* New Course Form */}
