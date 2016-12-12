0b01b15ff75eab611a3bea41de9b6e94c4bbd0cd
allenwhitedev <allenwhitedev@gmail.com>
2016-12-12 06:48:05 -0500

    Revert course selection removal to working version (last commit was breaking change)

--- src/components/CourseSelection.js
+++ src/components/CourseSelection.js
@@ -49,6 +49,7 @@ class CourseSelection extends Component
   handleOnRemove(event)
   {
     let index = event.target.id
+    console.log("index", index)
     this.state.courses.splice(index, 1);
     this.setState(this.state);
   }
@@ -61,6 +62,7 @@ class CourseSelection extends Component
   handleOnSubmit(event)
   {
     event.preventDefault()
+
     // adds new course to courses, reset newCourse state
     this.setState({ courses: this.state.courses.concat(this.state.newCourse), newCourse: "" })
   }
@@ -85,7 +87,7 @@ class CourseSelection extends Component
     let coursesDisplay = courses.map( (course, index) =>
       <li key={index} className="course">
         {course}
-        <a className="remove-course" id={index} onClick={this.handleOnRemove}> <i className="material-icons">close</i> </a>
+        <a className="remove-course" onClick={this.handleOnRemove}> <i className="material-icons" id={index}>close</i> </a>
       </li>
     )
 
@@ -106,7 +108,7 @@ class CourseSelection extends Component
         <footer className="submitButtonArea fullWidth textCenter">
           {submitButton}
         </footer>
-
+        
       </section>
     )
   }
