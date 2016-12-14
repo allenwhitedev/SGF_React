eba9d7f03434b376bf53fcbf0d5d8a0488fed914
allenwhitedev <allenwhitedev@gmail.com>
2016-12-13 20:20:46 -0500

    Add ui for join group button

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -17,7 +17,7 @@ class Groups extends Component
   {
     super(props)
 
-    this.handleClickGroup = this.handleClickGroup.bind(this)       
+    this.handleClickJoinGroup = this.handleClickJoinGroup.bind(this)       
 
     this.state = {groups: [], myGroups: []}
   }
@@ -42,7 +42,7 @@ class Groups extends Component
       return gravatar5                      
   }
 
-  handleClickGroup(group)
+  handleClickJoinGroup(group)
   {
     //let myGroups = this.state.myGroups
     //let groups = this.state.groups
@@ -78,31 +78,39 @@ class Groups extends Component
   {
     
     let groupsDisplay = this.state.groups.map((group, index) =>
-      <li key={group._id} className="group boxShadow1" id={index} onClick={ () => this.handleClickGroup({name: group.name, class: group.class, index: index}) }>
+      <li key={group._id} className="group boxShadow1" id={index}>
         <section className="groupTextInfo"> 
-          <Link to={"groups/" + group.name}>
-          <h2 className="groupName verticalAlignFlex"> <img className="gravatarSmall marginRight10px" src={this.getGravatar(group.gravatar)} alt={group.name}/> <span> {group.name} - {group.class} </span> </h2>
+          
+          <h2 className="groupName verticalAlignFlex"> 
+            <Link to={"groups/" + group.name}>
+              <section className="verticalAlignFlex"> 
+              <img className="gravatarSmall marginRight10px" src={this.getGravatar(group.gravatar)} alt={group.name}/> 
+              <span> {group.name} - {group.class} </span> 
+              </section>
+            </Link>
+            <button className="joinGroupButton rightJustifyFlex verticalAlignFlex tabPlusOnly" onClick={ () => this.handleClickJoinGroup({ groupId: group._id} ) }> 
+              JOIN GROUP &nbsp; 
+              <i className="material-icons">person_add</i> &nbsp;
+            </button>
+            <button className="joinGroupButton rightJustifyFlex verticalAlignFlex mobileOnly" onClick={ () => this.handleClickJoinGroup({ groupId: group._id} ) }> 
+              <i className="material-icons">person_add</i> &nbsp;
+            </button>
+          </h2>
           <h3>{group.description}</h3>
-          </Link>
+          
         </section>
 
-        <ul className="groupInfo tabPlusOnly fullWidth flexHorizontalLayout" id="">
-          <li className="groupMembers verticalAlignFlex"> <i className="material-icons">group</i> &nbsp; {group.members.length} members </li>
+        <ul className="groupInfo fullWidth flexHorizontalLayout" id="">
+          <li className="groupMembers tabPlusOnly verticalAlignFlex"> 
+          <i className="material-icons">group</i> &nbsp; {group.members.length} members </li>
           <li className="meetingTimes verticalAlignFlex"> <i className="material-icons">access_time</i> &nbsp; {group.meetingTimes} </li>
-          <li className="locationName verticalAlignFlex"> 
+          <li className="locationName tabPlusOnly verticalAlignFlex"> 
             <i className="material-icons">place</i> &nbsp;
             <span className="whiteText"> {group.locationName} </span> 
           </li>
         </ul>
       </li>
     )
-
-    // let myGroups = this.state.myGroups.map( (myGroup, index) =>
-    //   <li key={index} className="myGroup textCenter">
-    //     {myGroup.name} ({myGroup.class})
-    //     <br />
-    //   </li>
-    // )
 
     return(
      
